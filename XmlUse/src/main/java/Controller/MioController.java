11dac17cf6a069bace7c24f483e4d5bdcd4d0ac6
Michele.Genchi <Michele.Genchi@DESKTOP-MRESUTG>
2021-05-13 19:01:24 +0200

    Inizializzazione del controller in modo da leggere preventivamenti i dati dai file xml prima di scrivere su i nuovi dati cancellando i precendenti

--- XmlUse/src/main/java/Controller/MioController.java
+++ XmlUse/src/main/java/Controller/MioController.java
@@ -2,7 +2,6 @@ package Controller;
 
 import java.io.File;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.logging.Logger;
@@ -16,57 +15,62 @@ import Model.GregorianCalendarAdapter;
 import Model.Paziente;
 import Model.PazientiWrapper;
 
-public class Controller {
-	private static final Logger LOGGER = Logger.getLogger(Controller.class.getName());
+public class MioController {
+	private static final Logger LOGGER = Logger.getLogger(MioController.class.getName());
 	private final static String CLOUD_PATH_PAZIENTI="C:\\pazienti.xml";
 	private final static String CLOUD_PATH_APPUNTAMENTI="C:\\appuntamenti.xml";
+	private PazientiWrapper pazienti;
+	private AppuntamentiWrapper appuntamenti;
 
+	public MioController() {
+		pazienti=PazientiWrapper.getInstance();
+		appuntamenti=AppuntamentiWrapper.getInstance();
+		pazienti=leggi_Pazienti()!=null?leggi_Pazienti():pazienti;
+		appuntamenti=leggi_Appuntamenti()!=null?leggi_Appuntamenti():appuntamenti;
+	}
+	
 	public void scriviPaziente(Paziente paziente) {
-		PazientiWrapper ps=PazientiWrapper.getInstance();
-		ps.getLista().add(paziente);
-		//ps.setLista(Arrays.asList(p));
-
+		pazienti.getLista().add(paziente);
 		DaoXmlFile<PazientiWrapper> dao=new DaoXmlFile<>();
 		File file =new File(CLOUD_PATH_PAZIENTI);
-		boolean success=dao.salva(file, ps);
-		
+		boolean success=dao.salva(file, pazienti);
 		if (success) 
 			LOGGER.warning("Scrittura sul file pazienti.xml");
 		else
 			LOGGER.config("Scrittura sul file pazienti.xml fallita! ");
 	}
 
-	public List<Paziente> leggi_Pazienti() {
+	public PazientiWrapper leggi_Pazienti() {
 		DaoXmlFile<PazientiWrapper> dao=new DaoXmlFile<>();
 		File file =new File(CLOUD_PATH_PAZIENTI);
-		PazientiWrapper ps=dao.carica(file, PazientiWrapper.class);
-		if (ps.getLista().size()<=0)
-			LOGGER.config("non ci sono pazienti");	
-		return ps.getLista();
+		if (file.exists()) {
+			LOGGER.warning("Sono stati recuperati "+dao.carica(file, PazientiWrapper.class).getLista().size()+"  pazienti");
+			return dao.carica(file, PazientiWrapper.class);
+		}
+		LOGGER.config("Problemi nella lettura del file pazienti.xml");
+		return null;
 	}
 	
 	public void scriviAppuntamento(Appuntamento appuntamento) {
-		AppuntamentiWrapper apps=AppuntamentiWrapper.getInstance();
-		apps.getLista().add(appuntamento);
-		//ps.setLista(Arrays.asList(p));
-
+		appuntamenti.getLista().add(appuntamento);
 		DaoXmlFile<AppuntamentiWrapper> dao=new DaoXmlFile<>();
 		File file =new File(CLOUD_PATH_APPUNTAMENTI);
-		boolean success=dao.salva(file, apps);
-		
+		boolean success=dao.salva(file, appuntamenti);
 		if (success) 
 			LOGGER.warning("Scrittura su file appuntamenti.xml");
 		else
 			LOGGER.config("Scrittura su file appuntamenti.xml fallita! ");
 	}
 	
-	public List<Appuntamento> leggi_Appuntamenti() {
+	public AppuntamentiWrapper leggi_Appuntamenti() {
 		DaoXmlFile<AppuntamentiWrapper> dao=new DaoXmlFile<>();
 		File file =new File(CLOUD_PATH_APPUNTAMENTI);
-		AppuntamentiWrapper apps=dao.carica(file, AppuntamentiWrapper.class);
-		if (apps.getLista().size()<=0)
-			LOGGER.config("non ci sono appuntamenti");	
-		return apps.getLista();
+		if (file.exists()) {
+			LOGGER.warning("Sono stati recuperati "+dao.carica(file, AppuntamentiWrapper.class).getLista().size()+" appuntamenti");
+			return dao.carica(file, AppuntamentiWrapper.class);
+		}
+		LOGGER.config("Problemi nella lettura del file appuntamenti.xml");
+		return null;
 	}
 
 	private boolean confrontoDate(GregorianCalendar data1, GregorianCalendar data2) {
@@ -81,18 +85,26 @@ public class Controller {
 	@XmlJavaTypeAdapter(GregorianCalendarAdapter.class) 
 	public List<Appuntamento> appuntamentiPerData(GregorianCalendar data) {
 		List<Appuntamento> filter=null;
-		filter=leggi_Appuntamenti().stream().filter(a -> 
+		filter=appuntamenti.getLista().stream().filter(a -> 
 			confrontoDate(a.getData(), data)).collect(Collectors.toList());	
 		return filter;
 	}
 	
 	public List<Appuntamento> appuntamentiDelPaziente(int id_paziente) {
-		List<Appuntamento> filter=leggi_Appuntamenti().stream().filter(a->a.getIdPaziente()==id_paziente).collect(Collectors.toList());
+		List<Appuntamento> filter=appuntamenti.getLista().stream().filter(a->a.getIdPaziente()==id_paziente).collect(Collectors.toList());
 		return filter;
 	}
 	
 	public Paziente ricercaPerId(int id) {
-		return leggi_Pazienti().stream().filter(i->(i.getId()==id)).findFirst().get(); 
+		return pazienti.getLista().stream().filter(i->(i.getId()==id)).findFirst().get(); 
+	}
+
+	public PazientiWrapper getPazienti() {
+		return pazienti;
+	}
+
+	public AppuntamentiWrapper getAppuntamenti() {
+		return appuntamenti;
 	}
 
 
